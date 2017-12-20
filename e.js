c28916c06b711c6f7a92ab2994119e2e44413c20
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-20 21:06:07 +0700

    code

--- e.js
+++ e.js
@@ -8,8 +8,33 @@ function getRandom2(x, y) {
     return Math.floor(Math.random() * (y - x + 1)) + x
 }
 
-console.log(getRandom2(2, 3));
+// console.log(getRandom2(2, 3));
 // console.log(Math.random());
 // console.log(Math.round(10.5))
 // console.log(Math.ceil(0.1))
 // console.log(Math.floor(0.9))
+
+/*
+
+n = 4;
+
+*
+**
+***
+****
+
+*/
+function veHinh(n) {
+    for(let s = ''; s.length < n;) console.log(s += '*');
+}
+// function veHinh(n) {
+//     for (let i = 1; i <= n; i++) {
+//         let s = '';
+//         for (let j = 1; j <= n; j++) {
+//             if (i >= j) s += '*'
+//         }
+//         console.log(s);
+//     }
+// }
+
+veHinh(4);
